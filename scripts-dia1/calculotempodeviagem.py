140220175420687f5751a15ede40d3b795b3670b
lisboamab <lisboamab@gmail.com>
2022-02-16 23:33:33 -0300

    first commit

---
+++ scripts-dia1/calculotempodeviagem.py
@@ -0,0 +1,7 @@
+distancia = float(input("Digite a distância em km:"))
+
+velocidade_media = float(input("Digite a velocidade média em km/h: "))
+
+tempo_total_horas = distancia/velocidade_media
+
+print(f"\nO tempo estimado é de {tempo_total_horas:.0f} horas\n")
---
+++ scripts-dia1/perguntandonome.py
@@ -0,0 +1,10 @@
+import string
+
+
+nome = str(input("Qual é seu nome?"))
+
+idade = int(input("Qual é sua idade?"))
+
+idade2 = len(nome)
+
+print(f"\n Legal, seu nome tem {idade2} letras \n")
