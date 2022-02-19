3f1d4144cdbf8ccacb53e037bd926a775f532fdf
lisboamab <lisboamab@gmail.com>
2022-02-19 01:19:03 -0300

    Adiciona o dia 2

---
+++ scripts-dia2/exemplo.py
@@ -0,0 +1,16 @@
+# nota1 = float(input("\nDigite sua nota da primeira prova: \n"))
+
+# nota2 = float(input("\nDigite sua nota da segunda prova: \n"))
+
+# nota3 = float(input("\nDigite sua nota da terceira prova: \n"))
+
+# media = (nota1 + nota2 + nota3) / 3
+
+# media_arred = round(media, 2)
+
+nome = "Marcos Antonio Barros Lisboa"
+
+letra = input("Qual letra você quer contar?")
+
+print(f"Tem {nome.count(letra)} letras {letra} no seu nome")
+
