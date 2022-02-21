ed13886858ebb1cbbdfbb531d8d7d5c7337044fe
lisboamab <lisboamab@gmail.com>
2022-02-21 00:02:56 -0300

    Adiciona pasta de exercicios e responde os mesmos

---
+++ exercicios-python/cap-02/exercicios-dia-2.py
@@ -0,0 +1,48 @@
+# Exercicio 1
+
+print(10 + 20 * 30)
+
+print( 42 / 30 )
+
+print((94 + 2) * 6 - 1)
+
+# Exercicio 2
+
+print(int(10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2))
+
+# Exercicio 3
+
+print("Marcos Antonio Barros Lisboa")
+
+# Exercicio 4
+"""
+a = int(input("Insira aqui o valor de 'a': "))
+
+b = int(input("Insira aqui o valor de 'b': "))
+
+print(f"A resposta para a equação 2a * 3b sendo a = {a} e b = {b} é: {2*a*3*b} ")
+
+# Exercicio 5
+"""
+x = 10
+
+y = 18
+
+z = 16
+
+print(x + y + z)
+
+# Exercicio 6
+
+print("Olá vamos calcular o aumento sob o seu salário!")
+
+salario = int(input("Qual é o valor do seu salário? "))
+
+porcentagem = int(input("Qual é a porcentagem de aumento no seu salário? Escreva sem o '%' no final! ")) / 100
+
+aumento = int(salario * porcentagem)
+
+novo_salario = (int(salario * porcentagem) + salario)
+
+print(f"\nParabéns! Seu aumento vai ser de {aumento}! O seu novo salário é de {novo_salario} \n")
+
