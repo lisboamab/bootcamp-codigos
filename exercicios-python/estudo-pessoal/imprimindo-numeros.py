4a79f14c6535d65a87ac0935ad709170ca3ac0f2
lisboamab <lisboamab@gmail.com>
2022-05-13 11:31:24 -0300

    for in for

---
+++ exercicios-python/estudo-pessoal/imprimindo-numeros.py
@@ -0,0 +1,13 @@
+# // 1 - extra, imprimir todos os números dentro desse array:
+
+# // let array = [[1,9], [2,6], [3,4], [6,7]]
+
+# // Fazer o mesmo aqui:
+
+# // let array = [[1,9], [2,6], [3,4], [6,7, 3,4,5]]
+
+lista1 = [[1,9], [2,6], [3,4], [6,7]]
+
+for i in lista1:
+    for j in i:
+        print(j)
